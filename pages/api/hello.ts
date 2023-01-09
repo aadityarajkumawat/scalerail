58d0a386359896cb57dd154aa936b21d2922c77c
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-09 22:48:07 +0530

    cool

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -13,9 +13,9 @@ export default function handler(
 ) {
   const fileContents = readFileSync("./data.json", { encoding: "utf-8" });
   const data = JSON.parse(fileContents);
-  const out = execSync("ls", { encoding: "utf-8" })
-    .split("\n")
-    .filter((n) => !!n);
+  // const out = execSync("ls", { encoding: "utf-8" })
+  //   .split("\n")
+  //   .filter((n) => !!n);
 
-  res.status(200).json({ data, out });
+  res.status(200).json({ data, out: "none" });
 }
