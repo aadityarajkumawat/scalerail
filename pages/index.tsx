9e56eddd233e209e4a9ce87868a4b46609cf331a
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-09 23:04:59 +0530

    f

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { API_URL } from "../consts";
 
-// export async function getServerSideProps() {
-//   const res = await fetch(`${API_URL}/hello`);
-//   const body = await res.json();
-//   return { props: { data: body } };
-// }
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/hello`);
+  const data = await res.json();
+  return { props: { data } };
+}
 
-export default function Home(/**{ data }: any*/) {
-  return <div>{JSON.stringify({}, null, 2)}</div>;
+export default function Home({ data }: any) {
+  console.log(data);
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
