7a278ca3757dbdac97c05d6feeba481a577a7bf9
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-09 23:41:13 +0530

    woohoo

--- consts.ts
+++ consts.ts
@@ -1,4 +1,4 @@
 export const __is_prod__ = process.env.NODE_ENV === "production";
 export const API_URL = __is_prod__
-  ? `https://scalerail.vercel.app/api`
+  ? `https://${process.env.VERCEL_URL}/api`
   : "http://localhost:3000/api";
