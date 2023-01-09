a14113ce285bda2fd265770532446fc29208f810
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-09 21:32:22 +0530

    base done

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { exec } from "child_process";
+import { readFileSync } from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  heading: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const fileContents = readFileSync("./data.json", { encoding: "utf-8" });
+  const data = JSON.parse(fileContents);
+  const out = exec("ls", (err, stdout, stderr) => {
+    console.log(stdout, stderr);
+  });
+  res.status(200).json(data);
 }
