07fe443e4f96ca7354b38d9210808f5f257fed4e
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-12 22:57:15 +0530

    take a quick peek

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { sanityClient } from "../sanity";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 export async function getStaticProps() {
   const homePageContent = await sanityClient.fetch('*[_type=="home"][0]');
@@ -13,8 +14,22 @@ interface HomeProps {
 export default function Home({ heading, description }: HomeProps) {
   return (
     <div>
-      <h1>{heading}</h1>
-      <p>{description}</p>
+      <Parallax pages={2}>
+        <ParallaxLayer
+          className="flex flex-col items-center justify-center"
+          style={{
+            backgroundPosition: "center",
+            backgroundImage: `url(leaves_neon_triangle.jpg)`,
+            backgroundSize: "cover",
+          }}
+        >
+          <h2 className="text-center text-8xl">ScaleRail</h2>
+          <p className="py-2 text-lg">software | ops | action</p>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+          Welcome to scalerail
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
