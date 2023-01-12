c3dd9ff4c487a5fd9d23c82b3c7352940dde50c4
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-13 02:04:10 +0530

    fixed font error

--- centuryGothicFont.ts
+++
@@ -1,8 +0,0 @@
-import localFont from "@next/font/local";
-
-export const centuryGothic = localFont({
-  src: [
-    { path: "./pages/CenturyGothic.ttf", weight: "400", style: "normal" },
-    { path: "./pages/GOTHICB.ttf", weight: "500", style: "bold" },
-  ],
-});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { centuryGothic } from "../centuryGothicFont";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${centuryGothic.className} w-full h-full`}>
+    <main className={`w-full h-full`}>
       <Component {...pageProps} />
     </main>
   );
