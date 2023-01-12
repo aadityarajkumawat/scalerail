a19167e475d3ad799c75c13196f2ed755de580cf
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-12 23:32:23 +0530

    fixed it

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,18 +15,6 @@ export default function Home({ heading, description }: HomeProps) {
   return (
     <div>
       <Parallax pages={2}>
-        {/* <ParallaxLayer
-          speed={0.3}
-          className="flex flex-col items-center justify-center"
-          style={{
-            backgroundPosition: "center",
-            backgroundImage: "url(leaves_neon_triangle.jpg)",
-            backgroundSize: "cover",
-          }}
-        >
-          <h2 className="text-center text-8xl">ScaleRail</h2>
-          <p className="py-2 text-lg">software | ops | action</p>
-        </ParallaxLayer> */}
         <ParallaxLayer
           offset={0}
           speed={1}
@@ -36,9 +24,13 @@ export default function Home({ heading, description }: HomeProps) {
             backgroundSize: "cover",
           }}
         />
-        <ParallaxLayer offset={0} speed={0.5} className="flex justify-center">
+        <ParallaxLayer
+          offset={0}
+          speed={0.5}
+          className="flex justify-center items-center"
+        >
           <div
-            className="flex flex-col justify-center items-center"
+            className="flex flex-col justify-center items-center w-full h-80 flex-1"
             style={{
               background:
                 "linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, rgba(20, 20, 20, 0.621455) 49.08%, rgba(217, 217, 217, 0) 100%)",
