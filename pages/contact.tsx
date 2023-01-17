c01ce457504c8dea40bc410f9fa3168182e4c4ab
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-17 16:34:01 +0530

    updated home page and added contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,16 +1,324 @@
-export default function Contact() {
+import React, { useState } from "react";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+import flags from "react-phone-number-input/flags";
+import { MultiSelect } from "react-multi-select-component";
+import Select from "react-select";
+import z from "zod";
+
+const helpRequiringDomains = [
+  {
+    label: "(1) Project Mgmt - spec/build/launch tool to manage the big things",
+    value: "Project Mgmt",
+  },
+  {
+    label: "(2) Task Mgmt - spec/build/launch tool to manage the little things",
+    value: "Task Mgmt",
+  },
+  {
+    label: "(3) Software Dev - spec/build/launch custom programs",
+    value: "Software Dev",
+  },
+  {
+    label: "(4) Connect/Integrate - spec/build/launch API/Webhooks etc.",
+    value: "Connect/Integrate",
+  },
+  {
+    label: "(5) Database Dev - spec/build/launch storage systems",
+    value: "Database Dev",
+  },
+  {
+    label: "(6) Models/Algos - spec/build/launch custom analytics programs",
+    value: "Models/Algos",
+  },
+  {
+    label:
+      "(7) Automation Sprint - machines do machine stuff, people do people stuff",
+    value: "Automation Sprint",
+  },
+  {
+    label: "(8) QA/Testing - spec/build/lauch semi-automated testing programs",
+    value: "QA/Testing",
+  },
+  {
+    label: "(9) Market Analysis - structure/quantify your neighborhood",
+    value: "Market Analysis",
+  },
+  {
+    label: "(10) Expansion Strategy - now we take over neighborhood",
+    value: "Expansion Strategy",
+  },
+  {
+    label: "(11) Strategic Talent - find/screen/recommend absolute killers",
+    value: "Strategic Talent",
+  },
+  {
+    label: "(12) Bolt-On Talent - temp a ScaleRailer for dev/quant/ops/pm role",
+    value: "Bolt-On Talent",
+  },
+  {
+    label: "(13) Due Diligence - have ScaleRail fMRI before you buy",
+    value: "Due Diligence",
+  },
+  {
+    label: "(14) Capital Wayfinding - prep/connect in our PE/VC network",
+    value: "Capital Wayfinding",
+  },
+  {
+    label:
+      "(15) Aesthetics - videos, graphics, logos, and colors to tell your story",
+    value: "Aesthetics",
+  },
+];
+
+type Domains = typeof helpRequiringDomains[number];
+
+interface ContactFormProps {
+  name: string;
+  companyName: string;
+  email: string;
+  phone: string;
+  country: string;
+  helpRequiringDomains: Array<Domains>;
+  anythingElse: string;
+}
+
+interface InputProps {
+  id: string;
+  name: string;
+  label: string;
+  type: string;
+  value: string;
+  error: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+  className?: string;
+  textarea?: boolean;
+}
+
+function generateFormBodyStructure(
+  name: string,
+  email: { email: string; text: string },
+  companyName: string,
+  phoneData: { phone: string; countryShortName: string },
+  dropdown: Array<number>,
+  anythingElse: string
+) {
+  const localDate = new Date();
+  let timeZoneOffset = localDate.getTimezoneOffset();
+
+  const payload = {
+    answers: {
+      name,
+      email,
+      text0: companyName,
+      phone_1: phoneData,
+      dropdown,
+      long_text2: anythingElse,
+    },
+    "form-timezone-offset": timeZoneOffset,
+  };
+
+  return JSON.stringify(payload);
+}
+
+function Input(props: InputProps) {
   return (
-    <form
-      style={{ display: "flex", flexDirection: "column", width: "300px" }}
-      action="https://formsubmit.co/arkumawat78@gmail.com"
-      method="POST"
-    >
-      <input type="hidden" name="_captcha" value="false"></input>
-      <input type="hidden" name="_next" value="http://localhost:3000"></input>
-      <input type="text" name="name" placeholder="Name" required />
-      <input type="email" name="email" placeholder="Email" required />
-      <textarea name="featues" placeholder="Features" required />
-      <button type="submit">Send</button>
-    </form>
+    <div className={`relative inp w-full ${props.className ?? ""}`}>
+      {props.textarea ? (
+        <textarea name={props.name} id={props.id}></textarea>
+      ) : (
+        <input
+          className={`${
+            props.value ? "has-value" : ""
+          } border-b border-b-pleasant-blue w-full`}
+          type={props.type}
+          name={props.name}
+          id={props.id}
+          value={props.value}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+        />
+      )}
+      <label htmlFor={props.id} className="w-full">
+        {props.label}
+      </label>
+      <div className="absolute text-sm text-red-400">{props.error}</div>
+    </div>
+  );
+}
+
+const contactFormSchema = z.object({
+  name: z.string().min(1, { message: "Please enter a name" }),
+  companyName: z.string().min(1, { message: "Please enter a company name" }),
+  email: z.string().email({ message: "Please enter a valid email" }),
+  phone: z.string().min(10, { message: "Please enter a valid phone number" }),
+  country: z.string().min(1, { message: "Please enter a country" }),
+  helpRequiringDomains: z.array(z.string()),
+  anythingElse: z.string(),
+});
+
+export default function Contact() {
+  const [form, setForm] = useState<ContactFormProps>({
+    name: "",
+    companyName: "",
+    email: "",
+    phone: "",
+    country: "",
+    helpRequiringDomains: [],
+    anythingElse: "",
+  });
+
+  const [errors, setErrors] = useState<Record<keyof ContactFormProps, string>>({
+    anythingElse: "",
+    companyName: "",
+    country: "",
+    email: "",
+    helpRequiringDomains: "",
+    name: "",
+    phone: "",
+  });
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  }
+
+  function onBlur(e: React.FocusEvent<HTMLInputElement>) {
+    const { name, value } = e.target as {
+      name: keyof ContactFormProps;
+      value: string;
+    };
+
+    const vali = contactFormSchema.shape[name];
+
+    try {
+      vali.parse(value);
+      setErrors((ers) => ({ ...ers, [name]: "" }));
+    } catch (error) {
+      if (error instanceof Error) {
+        const msg = JSON.parse(error.message)[0].message;
+        setErrors((ers) => ({ ...ers, [name]: msg }));
+      }
+    }
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center py-10">
+      <h1 className="text-3xl font-bold mb-3">Contact</h1>
+      <p>Some text if you want to put something here</p>
+      <form className="mt-10 w-1/3 max-lg:w-1/2">
+        <div className="flex items-center justify-center gap-10 mb-10 w-full">
+          <Input
+            id="name"
+            label="Name"
+            name="name"
+            type="text"
+            value={form.name}
+            error={errors.name}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
+          <Input
+            id="email"
+            label="E-Mail"
+            name="email"
+            type="email"
+            value={form.email}
+            error={errors.email}
+            onChange={onChange}
+            onBlur={onBlur}
+          />
+        </div>
+        <Input
+          id="companyName"
+          label="Company Name"
+          name="companyName"
+          type="text"
+          value={form.companyName}
+          error={errors.companyName}
+          onChange={onChange}
+          className="mb-10"
+          onBlur={onBlur}
+        />
+
+        <div className="mb-10">
+          <PhoneInput
+            placeholder="Enter phone number"
+            className="border-b border-b-pleasant-blue w-full text-[#333]"
+            value={form.phone}
+            onCountryChange={(country: any) => {
+              setForm((f) => ({ ...f, country }));
+            }}
+            onChange={(phone: any) => {
+              setForm((f) => ({ ...f, phone }));
+            }}
+            onBlur={(e: any) => {
+              const phone = e.target.value;
+              try {
+                contactFormSchema.shape.phone.parse(phone);
+                setErrors((ers) => ({ ...ers, phone: "" }));
+              } catch (error) {
+                if (error instanceof Error) {
+                  const msg = JSON.parse(error.message)[0].message;
+                  setErrors((ers) => ({ ...ers, phone: msg }));
+                }
+              }
+            }}
+            flags={flags}
+          />
+          <div className="text-sm text-red-400">{errors.phone}</div>
+        </div>
+
+        <Select
+          isMulti
+          placeholder="What can we help out with?"
+          name="helpRequiringDomains"
+          className="bg-black select text-white mb-10"
+          options={helpRequiringDomains}
+          styles={{
+            multiValueLabel: () => ({
+              color: "white",
+              display: "flex",
+            }),
+            multiValue: () => ({
+              border: "1px solid #05a8f4",
+              borderRadius: "5px",
+              display: "flex",
+              marginRight: "1rem",
+            }),
+            control: () => ({
+              border: "1px solid #05a8f4",
+              display: "flex",
+              borderRadius: "5px",
+            }),
+          }}
+        />
+        <div className="w-full">
+          <label
+            htmlFor="anything-else"
+            className="mb-5"
+            style={{ position: "static" }}
+          >
+            Anything else you want to share?
+          </label>
+          <textarea
+            className="bg-black border border-pleasant-blue rounded-md w-full p-5 mt-3"
+            name="anythingElse"
+            id="anything-else"
+            cols={30}
+            rows={5}
+            // onChange={onCh}
+            // onBlur={onBlur}
+          ></textarea>
+        </div>
+        <button
+          className="w-full py-3 mt-10 rounded-lg bg-pleasant-blue text-white"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 import { sanityClient } from "../sanity";
 
 export async function getStaticProps() {
@@ -11,7 +12,29 @@ interface HomeProps {
   description: string;
 }
 
+function useScrollPosition() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrollPosition;
+}
+
 export default function Home({ heading, description }: HomeProps) {
+  const scroll = useScrollPosition();
+  console.log({ scroll });
+
   useEffect(() => {
     const parallax = document.getElementById("parallax");
 
@@ -27,112 +50,133 @@ export default function Home({ heading, description }: HomeProps) {
   }, []);
 
   return (
-    <section>
-      <div
-        id="parallax"
-        className="parallax-item"
-        style={{
-          backgroundSize: "cover",
-        }}
-      >
+    <div>
+      <div className="absolute w-full h-[80px]">
+        <div className="w-full flex justify-between bg-transparent px-10 py-5">
+          <div>
+            <img src="/logo.png" alt="" style={{ width: "40px" }} />
+          </div>
+          <div>
+            <ul className="flex items-center justify-center text-lg">
+              <li>about |</li>
+              <li>products |</li>
+              <li>
+                <Link href="/contact">contact</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <section>
         <div
-          id="shadow"
-          className="w-full h-full flex-col text-center flex justify-center items-center"
+          id="parallax"
+          className="parallax-item"
           style={{
-            background:
-              "linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, rgba(20, 20, 20, 0.621455) 49.08%, rgba(217, 217, 217, 0) 100%)",
+            backgroundSize: "cover",
           }}
         >
-          <h2 id="shadow" className="text-center text-7xl font-thin">
-            ScaleRail
-          </h2>
-          <p className="py-2 text-lg">software | ops | action</p>
+          <div
+            id="shadow"
+            className="w-full h-full flex-col text-center flex justify-center items-center"
+            style={{
+              background:
+                "linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, rgba(20, 20, 20, 0.621455) 49.08%, rgba(217, 217, 217, 0) 100%)",
+            }}
+          >
+            <div>
+              <h2 id="shadow" className="text-center text-7xl font-thin">
+                ScaleRail
+              </h2>
+              <p className="py-2 text-lg">software | ops | action</p>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="parallax-item abs" style={{ justifyContent: "start" }}>
-        <div className="absolute bg-black w-[500px] px-10 py-5 rounded-lg left-[3rem]">
-          <p className="text-pleasant-blue">
-            Imagination is the voice of daring. If there is anything Godlike
-            about God it is that He dared to imagine everything - Henry Miller
-          </p>
+        <div className="parallax-item abs" style={{ justifyContent: "start" }}>
+          <div className="absolute bg-black w-[500px] px-10 py-5 rounded-lg left-[3rem]">
+            <p className="text-pleasant-blue">
+              Imagination is the voice of daring. If there is anything Godlike
+              about God it is that He dared to imagine everything - Henry Miller
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="parallax-item">
-        <div className="max-w-3xl m-auto">
-          <h1 className="text-pleasant-blue text-5xl leading-snug mb-10">
-            We make tools for the most productive people on planet earth
-          </h1>
-          <p className="text-3xl text-pleasant-blue leading-snug">
-            The most productive human in the history of the world is alive today
-            - and we think he needs more than a spreadsheet.
-          </p>
-          <p className="my-10 text-lg">
-            Our tools are designed to maximize the most important metric at your
-            company &quot;average hours of focused attention&quot;
-          </p>
-          <p className="my-10 text-lg">
-            We&apos;re constantly innovating new solutions to make your workflow
-            easier and more efficient
-          </p>
-          <p className="my-10 text-lg">
-            We believe everyone should have access to powerful tools that enable
-            peak productivity. With our tools, you&apos;ll be able to work
-            smarter and faster than ever before
-          </p>
+        <div className="parallax-item">
+          <div className="max-w-3xl m-auto">
+            <h1 className="text-pleasant-blue text-5xl leading-snug mb-10">
+              We make tools for the most productive people on planet earth
+            </h1>
+            <p className="text-3xl text-pleasant-blue leading-snug">
+              The most productive human in the history of the world is alive
+              today - and we think he needs more than a spreadsheet.
+            </p>
+            <p className="my-10 text-lg">
+              Our tools are designed to maximize the most important metric at
+              your company &quot;average hours of focused attention&quot;
+            </p>
+            <p className="my-10 text-lg">
+              We&apos;re constantly innovating new solutions to make your
+              workflow easier and more efficient
+            </p>
+            <p className="my-10 text-lg">
+              We believe everyone should have access to powerful tools that
+              enable peak productivity. With our tools, you&apos;ll be able to
+              work smarter and faster than ever before
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="parallax-item abs" style={{ justifyContent: "start" }}>
-        <div className="absolute bg-black w-[500px] px-10 py-5 rounded-lg left-[3rem]">
-          <p className="text-pleasant-blue">
-            The future is something which everyone reaches at the rate of sixty
-            minutes an hour, whatever he does, whoever he is | C.S. Lewis
-          </p>
+        <div className="parallax-item abs" style={{ justifyContent: "start" }}>
+          <div className="absolute bg-black w-[500px] px-10 py-5 rounded-lg left-[3rem]">
+            <p className="text-pleasant-blue">
+              The future is something which everyone reaches at the rate of
+              sixty minutes an hour, whatever he does, whoever he is | C.S.
+              Lewis
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="parallax-item">
-        <div className="max-w-3xl m-auto">
-          <h1 className="text-pleasant-blue text-5xl leading-snug mb-10">
-            Creativity is the intersection region of knowledge + imagination
-          </h1>
-          <p className="text-3xl text-pleasant-blue leading-snug">
-            Our products do one thing: increase the area of this interaction
-            region
-          </p>
-          <p className="my-10 text-lg">
-            Our tools provide the bridge that lets you explore your creative
-            potential and unlock new possibilities
-          </p>
-          <p className="my-10 text-lg">
-            By combining intelligence, tools and technology, we allow you to
-            achieve your fullest potential and make unique connections that can
-            have positive impacts on your life
-          </p>
-          <p className="my-10 text-lg">
-            We are passionate about helping you explore the depths of your
-            creativity and use it to craft something incredible
-          </p>
+        <div className="parallax-item">
+          <div className="max-w-3xl m-auto">
+            <h1 className="text-pleasant-blue text-5xl leading-snug mb-10">
+              Creativity is the intersection region of knowledge + imagination
+            </h1>
+            <p className="text-3xl text-pleasant-blue leading-snug">
+              Our products do one thing: increase the area of this interaction
+              region
+            </p>
+            <p className="my-10 text-lg">
+              Our tools provide the bridge that lets you explore your creative
+              potential and unlock new possibilities
+            </p>
+            <p className="my-10 text-lg">
+              By combining intelligence, tools and technology, we allow you to
+              achieve your fullest potential and make unique connections that
+              can have positive impacts on your life
+            </p>
+            <p className="my-10 text-lg">
+              We are passionate about helping you explore the depths of your
+              creativity and use it to craft something incredible
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="parallax-item abs" style={{ justifyContent: "start" }}>
-        <div className="absolute bg-black w-[500px] px-10 py-5 rounded-lg left-[3rem]">
-          <p className="text-pleasant-blue">
-            The true object of all human life is play. Earth is a task garden;
-            heaven a playground | GK Chesterton
-          </p>
+        <div className="parallax-item abs" style={{ justifyContent: "start" }}>
+          <div className="absolute bg-black w-[500px] px-10 py-5 rounded-lg left-[3rem]">
+            <p className="text-pleasant-blue">
+              The true object of all human life is play. Earth is a task garden;
+              heaven a playground | GK Chesterton
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="parallax-item py-24">
-        <div className="max-w-3xl m-auto">
-          <button className="border-2 px-4 py-1 rounded-full border-[#ffffff70] text-[#ffffff70]">
-            Appreciate
-          </button>
+        <div className="parallax-item py-24">
+          <div className="max-w-3xl m-auto">
+            <button className="border-2 px-4 py-1 rounded-full border-[#ffffff70] text-[#ffffff70]">
+              Appreciate
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="flex justify-center items-center bg-[#444] py-4 text-gray-200 gap-3 text-xs">
-        <p>Terms of Service</p>
-        <p>Privacy Policy</p>
-        <p>Report Abuse</p>
-      </div>
-    </section>
+        <div className="flex justify-center items-center bg-[#444] py-4 text-gray-200 gap-3 text-xs">
+          <p>Terms of Service</p>
+          <p>Privacy Policy</p>
+          <p>Report Abuse</p>
+        </div>
+      </section>
+    </div>
   );
 }
