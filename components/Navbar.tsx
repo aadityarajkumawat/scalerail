961f5ba506e3d8f90fee2fb7b6a8826b836de9a3
Aditya Raj Kumawat <arkumawat78@gmail.com>
2023-01-23 13:49:12 +0530

    responsive ness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,13 +8,13 @@ export function Navbar() {
 
   return (
     <div className={`${isHome ? "absolute z-50" : ""} w-full h-[80px] inset-0`}>
-      <div className="w-full flex justify-between bg-transparent px-10 py-5">
+      <div className="w-full flex justify-between bg-transparent px-10 py-5 max-sm:px-5">
         <div>
           <Link href="/">
             <img
               src="/logo.png"
               alt="scalerail"
-              className="w-[40px] max-lg:w-[30px]"
+              className="w-[40px] max-lg:w-[30px] max-sm:w-[20px]"
             />
           </Link>
         </div>
